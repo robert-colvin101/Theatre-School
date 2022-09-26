0a306e87fc717ade3b0016a3ba00ac30e57aa1bc
Robert Colvin <robert_colvin@hotmail.co.uk>
2022-09-26 16:15:31 +0100

    join us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'profile', to: 'pages#profile'
+  get 'timetable', to: 'pages#timetable'
+  get 'joinus', to: 'pages#joinus'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
