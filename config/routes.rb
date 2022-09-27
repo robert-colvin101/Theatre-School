dbe04416f7e0f3bd6423706f71d173b7f4dd54b9
Robert Colvin <robert_colvin@hotmail.co.uk>
2022-09-27 11:26:54 +0100

    faqs

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'profile', to: 'pages#profile'
   get 'timetable', to: 'pages#timetable'
   get 'joinus', to: 'pages#joinus'
+  get 'faqs', to: 'pages#faqs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
