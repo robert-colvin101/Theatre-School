4326801532320b45dcc35ace275b8105f87e4b7f
Robert Colvin <robert_colvin@hotmail.co.uk>
2022-10-04 14:43:23 +0100

    links page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,15 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
