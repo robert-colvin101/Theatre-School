2475fb315ca44abc6ef992e9207e97780b8c4bfd
Robert Colvin <robert_colvin@hotmail.co.uk>
2022-10-04 16:08:27 +0100

    work on course structure side bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,25 @@
 class PagesController < ApplicationController
   def home
+
   end
   def timetable
   end
   def profile
     @user = current_user
+  end
+  def mondaywk1
+    @course = true
+  end
+  def tuesdaywk1
+    @course = true
+  end
+  def wednesdaywk1
+    @course = true
+  end
+  def thursdaywk1
+    @course = true
+  end
+  def fridaywk1
+    @course = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   get 'timetable', to: 'pages#timetable'
   get 'joinus', to: 'pages#joinus'
   get 'faqs', to: 'pages#faqs'
+  get 'mondaywk1', to: 'pages#mondaywk1'
+  get 'tuesdaywk1', to: 'pages#tuesdaywk1'
+  get 'wednesdaywk1', to: 'pages#wednesdaywk1'
+  get 'thursdaywk1', to: 'pages#thursdaywk1'
+  get 'fridaywk1', to: 'pages#fridaywk1'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
