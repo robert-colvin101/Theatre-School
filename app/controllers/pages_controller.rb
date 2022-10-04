9783e43bc205aeaa1a7ac7cdd1bd582b44a0b4b0
Robert Colvin <robert_colvin@hotmail.co.uk>
2022-10-04 16:25:19 +0100

    added course lyout for each week and day

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
   def home
-
+    @course = false
   end
   def timetable
+    @course = false
   end
   def profile
     @user = current_user
@@ -20,6 +21,36 @@ class PagesController < ApplicationController
     @course = true
   end
   def fridaywk1
+    @course = true
+  end
+  def mondaywk2
+    @course = true
+  end
+  def tuesdaywk2
+    @course = true
+  end
+  def wednesdaywk2
+    @course = true
+  end
+  def thursdaywk2
+    @course = true
+  end
+  def fridaywk2
+    @course = true
+  end
+  def mondaywk3
+    @course = true
+  end
+  def tuesdaywk3
+    @course = true
+  end
+  def wednesdaywk3
+    @course = true
+  end
+  def thursdaywk3
+    @course = true
+  end
+  def fridaywk3
     @course = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,15 @@ Rails.application.routes.draw do
   get 'wednesdaywk1', to: 'pages#wednesdaywk1'
   get 'thursdaywk1', to: 'pages#thursdaywk1'
   get 'fridaywk1', to: 'pages#fridaywk1'
+  get 'mondaywk2', to: 'pages#mondaywk2'
+  get 'tuesdaywk2', to: 'pages#tuesdaywk2'
+  get 'wednesdaywk2', to: 'pages#wednesdaywk2'
+  get 'thursdaywk2', to: 'pages#thursdaywk2'
+  get 'fridaywk2', to: 'pages#fridaywk2'
+  get 'mondaywk3', to: 'pages#mondaywk3'
+  get 'tuesdaywk3', to: 'pages#tuesdaywk3'
+  get 'wednesdaywk3', to: 'pages#wednesdaywk3'
+  get 'thursdaywk3', to: 'pages#thursdaywk3'
+  get 'fridaywk3', to: 'pages#fridaywk3'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
